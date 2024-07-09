21e0142368e89bbf51e939e711b54e4c3c58528c
Akshaya <akshayam1712@gmail.com>
2024-07-09 09:53:11 +0530

    created a 2048 game to python projects

---
+++ 2048_game/2048_game.py
@@ -0,0 +1,47 @@
+import logic
+
+if __name__=='__main__':
+    mat=logic.start_game()
+    
+while(True):
+    x=input("Pressthe command: ")
+    if(x=='W' or x=='w'):
+        mat,flag=logic.move_up(mat)
+        status=logic.current_state_matrix(mat)
+        print(status)
+        if status=='Game not over':
+            logic.add_new2(mat)
+        else:
+            break
+        
+    elif(x=='S' or x=='s'):
+        mat,flag=logic.move_down(mat)
+        status=logic.current_state_matrix(mat)
+        print(status)
+        if status=='Game not over':
+            logic.add_new2(mat)
+        else:
+            break
+        
+    elif(x=='A' or x=='a'):
+        mat,flag=logic.move_left(mat)
+        status=logic.current_state_matrix(mat)
+        print(status)
+        if status=='Game not over':
+            logic.add_new2(mat)
+        else:
+            break
+        
+    elif(x=='D' or x=='d'):
+        mat,flag=logic.move_right(mat)
+        status=logic.current_state_matrix(mat)
+        print(status)
+        if status=='Game not over':
+            logic.add_new2(mat)
+        else:
+            break
+        
+    else:
+        print("Invalid key pressed")
+    
+    print(mat)
--- README.md
+++ README.md
@@ -3,3 +3,5 @@ All python learnings:
   1. Number guessing game in Python - 17/6/2024
   2. Word guessing game
   3. 21-number-game
+  4. basic_calculator
+  5. 2048 game
